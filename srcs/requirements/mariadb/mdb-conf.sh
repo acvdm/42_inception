4ed0d26a2a6bd05abdd667f87c128f0255a0c44b
anvander <anvander@Docker.myguest.virtualbox.org>
2025-06-24 16:54:37 +0200

    corrections .conf

--- srcs/requirements/mariadb/mdb-conf.sh
+++ srcs/requirements/mariadb/mdb-conf.sh
@@ -1,17 +1,72 @@
 #!/bin/bash
-set -e
+
+# Vérifier que les variables d'environnement sont définies
+if [ -z "$SQL_DATABASE" ] || [ -z "$SQL_USER" ] || [ -z "$SQL_PWD" ] || [ -z "$SQL_ROOT_PWD" ]; then
+    echo "ERREUR: Variables d'environnement manquantes (SQL_DATABASE, SQL_USER, SQL_PWD, SQL_ROOT_PWD)"
+    exit 1
+fi
+
+#------------------------------------------------------
+pkill mysqld || true
+pkill mariadb || true
+sleep 2
+
+if pgrep mysqld > /dev/null; then
+    echo "ERREUR: mysqld est toujours live"
+fi
+
+if pgrep mariadb > /dev/null; then
+    echo "ERREUR: mariadb est toujours live"
+fi
 
 #-----------------------START  MARIADB
-service mariadb start
-sleep 5
+#service mariadb start
+#sleep 5
+echo "Demarrage de Mdb en mode securise..."
+mysqld_safe --skip-grant-tables --skip-networking &
+MYSQL_PID=$!
+sleep 10
+
+if ! pgrep mysqld > /dev/null; then
+    echo "ERREUR: impossible de demarrer MDB"
+    exit 1
+fi
 
 #-----------------------CONFIG MARIADB
-mariadb -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
-mariadb -e "CREATE USER IF NOT EXISTS '$SQL_USER'@wordpress IDENTIFIED BY '$SQL_PWD';"
-mariadb -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@wordpress IDENTIFIED BY '$SQL_PWD';"
-mariadb -e "FLUSH PRIVILEGES;"
+#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PWD';"
+#mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
+#mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PWD';"
+#mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';"
+#mysql -e "FLUSH PRIVILEGES;"
+
+echo "Configuration de la base de donnees..."
+mysql -u root << EOF
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PWD';
+CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;
+CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PWD';
+GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+if [ $? -ne 0 ]; then
+    echo "ERREUR: Echec de la configuration MDB"
+    kill $MYSQL_PID
+    exit 1
+fi
+
+echo "Configuration de la base de donnees reussie..."
 
 #-----------------------RESTART MARIADB 
-mysqladmin -u root -p$SQL_ROOT_PWD shutdown
-mysqld_safe --port=3306 --bin-address=0.0.0.0 --datadir='/var/lib/mysql'
+echo "Redemarrage MDB en mode normal"
+kill $MYSQL_PID
+sleep 5
+
+pkill mysqld ||true
+pkill mariadb || true
+sleep 2
+
+
+#mysqladmin -u root -p$SQL_ROOT_PWD shutdown
+exec mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
 #exec mysqld_safe --datadir='/var/lib/mysql'
--- srcs/requirements/wordpress/wp_conf.sh
+++ srcs/requirements/wordpress/wp_conf.sh
@@ -1,43 +1,48 @@
 #!/bin/bash
 
-set -e
-
 #-----------------------INSTALL WP-CLI
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/bin/wp
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+    echo "Installation de Wordpress..."
 
-#-----------------------PREPARATION WORDPRESS DIRECTORY
-mkdir -p /var/www/wordpress
-cd /var/www/wordpress
-chmod -R 755 /var/www/wordpress
-chown -R www-data:www-data /var/www/wordpress
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/bin/wp
 
-#-----------------------INSTALL WORDPRESS
-wp core download --allow-root
+    #-----------------------PREPARATION WORDPRESS DIRECTORY
+    mkdir -p /var/www/wordpress
+    cd /var/www/wordpress
+    chmod -R 755 /var/www/wordpress
+    chown -R www-data:www-data /var/www/wordpress
 
-# configuration wp-config.php 
-wp core config \
-    --dbhost="mariadb:3306" \
-    --dbname="$SQL_DATABASE" \
-    --dbuser="$SQL_USER" \
-    --dbpass="$SQL_PWD" \
-    --allow-root 
+    #-----------------------INSTALL WORDPRESS
+    wp core download --allow-root
 
-# wordpress installation + configuration
-wp core install \
-    --url="$WP_DOMAIN_NAME" \
-    --title="$WP_TITLE" \
-    --admin_user="$WP_ADMIN_NAME" \
-    --admin_password="$WP_ADMIN_PWD" \
-    --admin_email="$WP_ADMIN_EMAIL" \
-    --allow-root
+    # configuration wp-config.php 
+    wp core config \
+        --dbhost="mariadb:3306" \
+        --dbname="$SQL_DATABASE" \
+        --dbuser="$SQL_USER" \
+        --dbpass="$SQL_PWD" \
+        --allow-root 
 
-# create a new user
-wp user create "$WP_USER_NAME" "$WP_USER_EMAIL" \
-    --user-pass="$WP_USER_PWD" \
-    --role="$WP_USER_ROLE" \
-    --allow-root
+    # wordpress installation + configuration
+    wp core install \
+        --url="$WP_DOMAIN_NAME" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_NAME" \
+        --admin_password="$WP_ADMIN_PWD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --allow-root
+
+    # create a new user
+    wp user create "$WP_USER_NAME" "$WP_USER_EMAIL" \
+        --user-pass="$WP_USER_PWD" \
+        --role="$WP_USER_ROLE" \
+        --allow-root
+    echo "Wordpress installe avec succes !"
+else
+    echo "Wordpress deja configure, damarrage..."
+fi
 
 #-----------------------PHP-FPM CONFIGURATION
 # modificate line 36 of www.conf to listen = 9000
